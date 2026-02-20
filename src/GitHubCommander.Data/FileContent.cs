1cde5dc7619453f8c423298379f90cfd8cdcee1f
4090rt <artem2007yannurow@gmail.com>
2026-02-20 19:51:32 +0300

    Add files via upload

---
+++ src/GitHubCommander.Data/FileContent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GithubComander.src.GitHubCommander.Data
+{
+    public class FileContent
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("encoding")]
+        public string Encoding { get; set; }
+
+        public string GetDecodedContent()
+        {
+            if (string.IsNullOrEmpty(Content) || Encoding != "base64")
+                return string.Empty;
+
+            // GitHub добавляет переносы строк в base64, их нужно убрать
+            var cleanBase64 = Content.Replace("\n", "").Replace("\r", "");
+            var bytes = Convert.FromBase64String(cleanBase64);
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
---
+++ src/GitHubCommander.Data/RepositoryContent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GithubComander.src.GitHubCommander.Data
+{
+    public class RepositoryContent
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("size")]
+        public long Size { get; set; }
+
+        public bool IsDirectory => Type == "dir";
+        public bool IsFile => Type == "file";
+    }
+}
