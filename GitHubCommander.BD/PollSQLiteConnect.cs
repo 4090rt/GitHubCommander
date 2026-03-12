911972137240e1c90afed9e6cdbed0ccc7fa88b4
4090rt <artem2007yannurow@gmail.com>
2026-03-12 19:15:26 +0300

    Add files via upload

---
+++ GitHubCommander.BD/PollSQLiteConnect.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubComander.src.GitHubCommander.BD
+{
+    public class PollSQLiteConnect
+    {
+        private readonly Stack<SQLiteConnection> _available = new Stack<SQLiteConnection>();
+        private readonly List<SQLiteConnection> _inUse = new List<SQLiteConnection>();
+        private readonly object _lock = new object();
+        private readonly string _dbpath;
+        private readonly int _maxCouhnt = 10;
+        private readonly ILogger _loggr;
+
+        public PollSQLiteConnect(ILogger loggr)
+        {
+            BDPathClass dbpath = new BDPathClass();
+            _dbpath = dbpath.dbpath();
+            _loggr = loggr;
+        }
+
+        public SQLiteConnection CreatePoolConnect()
+        {
+            try
+            {
+                var connect = new SQLiteConnection($"DATA SOURCE={_dbpath}");
+                connect.Open();
+                return connect;
+            }
+            catch (SQLiteException ex)
+            {
+                _loggr.LogError("Возникло исключение " + ex.Message + ex.StackTrace);
+                throw;
+            }
+        }
+
+        public SQLiteConnection PoolOpen()
+        {
+            try
+            {
+                lock (_lock)
+                {
+                    SQLiteConnection connection;
+
+                    if (_available.Count > 0)
+                    {
+                        connection = _available.Pop();
+                        if (connection == null)
+                        {
+                            connection = CreatePoolConnect();
+                        }
+                        if (connection.State != System.Data.ConnectionState.Open)
+                        {
+                            connection = CreatePoolConnect();
+                        }
+                    }
+                    else if (_inUse.Count < _maxCouhnt)
+                    {
+                        connection = CreatePoolConnect();
+                    }
+                    else
+                    {
+                        throw new Exception("Пул занят!");
+                    }
+
+                    _inUse.Add(connection);
+                    return connection;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                _loggr.LogError("Возникло исключение " + ex.Message + ex.StackTrace);
+                throw;
+            }
+        }
+
+        public void PullClose(SQLiteConnection connection)
+        {
+            if (connection == null) return;
+            try
+            {
+                lock (_lock)
+                {
+                    if (_inUse.Contains(connection))
+                    {
+                        _inUse.Remove(connection);
+
+                        if (connection.State == System.Data.ConnectionState.Open)
+                        {
+                            _available.Push(connection);
+                        }
+                        else
+                        {
+                            connection.Dispose();
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Соединение не найдено");
+                    }
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                _loggr.LogError("Возникло исключение " + ex.Message + ex.StackTrace);
+                throw;
+            }
+        }
+    }
+}
---
+++ GitHubCommander.BD/SaveCommandLog.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace GithubComander.src.GitHubCommander.BD
+{
+    public class SaveCommandLog
+    {
+        private readonly Microsoft.Extensions.Logging.ILogger _logger;
+        private readonly PollSQLiteConnect _pollSQLiteConnect;
+        public SaveCommandLog(Microsoft.Extensions.Logging.ILogger logger, PollSQLiteConnect pollSQLiteConnect)
+        {
+            _logger = logger;
+            _pollSQLiteConnect = pollSQLiteConnect;
+        }
+
+        public async Task SaveLogg(IEnumerable<(string loginfo, DateTime dateTime)> logs)
+        {
+            if (logs == null || !logs.Any()) return;
+
+            SQLiteConnection connection = null;
+            SQLiteTransaction sQLiteTransaction = null;
+            try
+            {
+                _logger.LogInformation("Сохранение логов...");
+                _logger.LogInformation($"Пакетное сохранение {logs.Count()} логов...");
+
+                connection = _pollSQLiteConnect.PoolOpen();
+                sQLiteTransaction = connection.BeginTransaction();
+
+                string command = "INSERT INTO [LogBase] (Log, Date) VALUES(@L, @D)";
+
+                using (var commandsql = new SQLiteCommand(command, connection, sQLiteTransaction))
+                {
+                    commandsql.Parameters.Add("@L", DbType.String);
+                    commandsql.Parameters.Add("@D", DbType.DateTime);
+
+                    foreach (var log in logs)
+                    {
+                        commandsql.Parameters["@L"].Value = log.loginfo;
+                        commandsql.Parameters["@D"].Value = log.dateTime;
+                        await commandsql.ExecuteNonQueryAsync().ConfigureAwait(false);                  
+                    }
+                }
+                sQLiteTransaction.Commit();
+                _logger.LogInformation($"Успешно добавлено {logs.Count()} записей");
+            }
+            catch (SQLiteException ex)
+            {
+                sQLiteTransaction?.Rollback();
+                _logger.LogError($"Возникло исключение при работе с БД" + ex.Message + ex.StackTrace);
+                return;
+            }
+            catch (Exception ex)
+            {
+                sQLiteTransaction?.Rollback();
+                _logger.LogError("ВОзникло исключение" + ex.Message + ex.StackTrace);
+                return;
+            }
+            finally
+            {
+                sQLiteTransaction?.Dispose();
+                if (connection != null)
+                {
+                   _pollSQLiteConnect.PullClose(connection);
+                }
+            }
+        }
+    }
+}
